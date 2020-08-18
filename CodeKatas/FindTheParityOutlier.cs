ddc2f71b4fe6ac528e87fb8f04c043b298e92310
RealMarkSussex <mark.sussex@gocompare.com>
2020-08-18 11:28:33 +0100

    find parity outlier works

---
+++ CodeKatas/FindTheParityOutlier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKatas
+{
+    class FindTheParityOutlier
+    {
+        public static int Find(int[] integers)
+        {
+            var numOfEven = integers.Count(integer => integer % 2 == 0);
+            return numOfEven == 1 ? 
+                integers.First(integer => integer % 2 == 0) : integers.First(integer => integer % 2 != 0);
+        }
+    }
+}
