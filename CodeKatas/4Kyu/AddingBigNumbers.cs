48286a6684a231381edcf8e7ce61530d42815019
RealMarkSussex <mark.sussex@gocompare.com>
2020-08-19 09:14:36 +0100

    Adding big numbers

---
+++ CodeKatas/4Kyu/AddingBigNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace CodeKatas._4Kyu
+{
+    class AddingBigNumbers
+    {
+        public static string Add(string a, string b)
+        {
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+    }
+}
