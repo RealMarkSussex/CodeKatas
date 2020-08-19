2e9fb823e6cb2894619eed84ebeba747c8d14043
RealMarkSussex <mark.sussex@gocompare.com>
2020-08-19 15:29:52 +0100

    matrix calculator!!!!

---
+++ CodeKatas/4Kyu/MatrixDeterminant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKatas._4Kyu
+{
+    class MatrixDeterminant
+    {
+        public static int Determinant(int[][] matrix)
+        {
+            if (matrix.Length == 1)
+            {
+                return matrix[0][0];
+            }
+
+            var result = 0;
+            var matrixSize = matrix[0].Length;
+            for (var i = 0; i < matrixSize; i++)
+            {
+                for (var j = 0; j < matrixSize; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        result += Result(matrix, i, j);
+                    }
+                    else
+                    {
+                        result -= Result(matrix, i, j);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static int Result(int[][] matrix, int i, int j)
+        {
+            var determinant = Determinant(matrix.Where((arr, k) => k != i) //skip row#i
+                .Select(arr => arr.Where((item, k) => k != j) //skip col#j
+                    .ToArray())
+                .ToArray());
+            var value = matrix[0][j];
+            return value * determinant;
+        }
+    }
+}
--- CodeKatas/Program.cs
+++ CodeKatas/Program.cs
@@ -20,20 +20,8 @@ namespace CodeKatas
             PrintPickPeaks(plateauNoPeak, "Test case that is a plateau that is not a peak");
             Console.WriteLine(PrimesInNumbers.Factors(7775460));
             Console.WriteLine(IntegersRecreationOne.ListSquared(1, 250));
-            var top3Words = MostFrequentUsed.Top3("In a village of La Mancha, the name of which I have no desire to call to\",\r\n                  \"mind, there lived not long since one of those gentlemen that keep a lance\",\r\n                  \"in the lance-rack, an old buckler, a lean hack, and a greyhound for\",\r\n                  \"coursing. An olla of rather more beef than mutton, a salad on most\",\r\n                  \"nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra\",\r\n                  \"on Sundays, made away with three-quarters of his income.");
-            Console.WriteLine();
-            var i = 0;
-            foreach (var word in top3Words)
-            {
-                Console.Write(word + " ");
-                if (i == 10)
-                {
-                    Console.WriteLine();
-                    i = 0;
-                }
-
-                i++;
-            }
+            Console.WriteLine(MatrixDeterminant.Determinant(new int[][] { new[] { 1, 3, 5, 9 }, new[] { 1, 3, 1, 7 }, new[] { 4, 3, 9, 7 }, new []{ 5, 2, 0, 9} }));
+            Console.WriteLine(MatrixDeterminant.Determinant(new int[][] { new[] { 2, 5, 3 }, new[] { 1, -2, -1 }, new[] { 1, 3, 4 } }));
         }
 
         private static void PrintPickPeaks(Dictionary<string, List<int>> result, string testCase)
