44929fdb77b1f8f6979d259c424d3e8808adc15a
RealMarkSussex <mark.sussex@gocompare.com>
2020-08-20 11:27:44 +0100

    completed where my anagram at

---
+++ CodeKatas/5Kyu/WhereMyAnagramsAt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKatas._5Kyu
+{
+    class WhereMyAnagramsAt
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            var result = new List<string>();
+            var wordDictionary = WordToDictionary(word);
+            var wordsDictionaryList = words.Select(WordToDictionary).ToList();
+            for (var index = 0; index < wordsDictionaryList.Count; index++)
+            {
+                var potentialAnagramDictionary = wordsDictionaryList[index];
+                var isAnagram = true;
+                if (potentialAnagramDictionary.Sum(pa => pa.Value) != word.Length)
+                {
+                    continue;
+                }
+                foreach (var (key, value) in potentialAnagramDictionary)
+                {
+                    if (!wordDictionary.ContainsKey(key))
+                    {
+                        isAnagram = false;
+                    }
+                    else if(wordDictionary[key] != value)
+                    {
+                        isAnagram = false;
+                    }
+                }
+
+                if (isAnagram)
+                {
+                    result.Add(words[index]);
+                }
+            }
+
+            return result;
+        }
+
+        private static Dictionary<char, int> WordToDictionary(string word)
+        {
+            var dictionary = new Dictionary<char, int>();
+            foreach (var letter in word)
+            {
+                var success = dictionary.TryAdd(letter, 1);
+                if (!success)
+                {
+                    dictionary[letter]++;
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
--- CodeKatas/Program.cs
+++ CodeKatas/Program.cs
@@ -10,46 +10,12 @@ namespace CodeKatas
     {
         static void Main(string[] args)
         {
-            var simple = PickPeaks.GetPeaks(new[] { 0, 1, 2, 5, 1, 0 });
-            var harder = PickPeaks.GetPeaks(new[] { 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3 });
-            var plateauPeak = PickPeaks.GetPeaks(new[] { 1, 2, 2, 2, 1 });
-            var plateauNoPeak = PickPeaks.GetPeaks(new[] { 1, 2, 2, 2, 3 });
-            PrintPickPeaks(simple, "Simple test case");
-            PrintPickPeaks(harder, "Harder test case");
-            PrintPickPeaks(plateauPeak, "Test case with a plateau that is a peak");
-            PrintPickPeaks(plateauNoPeak, "Test case that is a plateau that is not a peak");
-            Console.WriteLine(PrimesInNumbers.Factors(7775460));
-            Console.WriteLine(IntegersRecreationOne.ListSquared(1, 250));
-            Console.WriteLine(MatrixDeterminant.Determinant(new int[][] { new[] { 1, 3, 5, 9 }, new[] { 1, 3, 1, 7 }, new[] { 4, 3, 9, 7 }, new []{ 5, 2, 0, 9} }));
-            Console.WriteLine(MatrixDeterminant.Determinant(new int[][] { new[] { 2, 5, 3 }, new[] { 1, -2, -1 }, new[] { 1, 3, 4 } }));
-            //var snailSort = SnailSort.Snail(new int[][]
-            //{
-            //    new[] {1, 2, 3},
-            //    new[] {4, 5, 6},
-            //    new[] {7, 8, 9}
-            //});
-            var snailSort = SnailSort.Snail(new int[0][]);
-            Console.WriteLine();
-            foreach (var num in snailSort)
+            var anagrams = WhereMyAnagramsAt.Anagrams("racer",
+                new List<string>
+                    {"carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr"});
+            foreach (var anagram in anagrams)
             {
-                Console.Write(num + " ");
-            }
-        }
-
-        private static void PrintPickPeaks(Dictionary<string, List<int>> result, string testCase)
-        {
-            Console.WriteLine("\n------------------------------------------------------");
-            Console.WriteLine($"{testCase}");
-            Console.WriteLine("pos => ");
-            foreach (var number in result["pos"])
-            {
-                Console.Write($", {number}");
-            }
-
-            Console.WriteLine("\npeaks => ");
-            foreach (var number in result["peaks"])
-            {
-                Console.Write($", {number}");
+                Console.WriteLine(anagram);
             }
         }
     }
