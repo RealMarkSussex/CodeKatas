56c8a537382917d64795b7af76f82a1236d76e9d
RealMarkSussex <mark.sussex@gocompare.com>
2020-08-20 13:56:00 +0100

    finished weight for weight

---
+++ CodeKatas/5Kyu/WeightForWeight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeKatas._5Kyu
+{
+    class WeightForWeight
+    {
+        public static string OrderWeight(string strng)
+        {
+            var numbers = strng.Split(" ");
+            Array.Sort(numbers, string.Compare);
+            var orderedNumbers = numbers
+                .OrderBy(num => num.Sum(l => int.Parse(l.ToString()))).ToArray();
+            return orderedNumbers
+                .Aggregate("", (current, orderedNumber) => current + (orderedNumber + " "))
+                .Trim();
+        }
+    }
+}
--- CodeKatas/Program.cs
+++ CodeKatas/Program.cs
@@ -17,6 +17,8 @@ namespace CodeKatas
             {
                 Console.WriteLine(anagram);
             }
+
+            Console.WriteLine(WeightForWeight.OrderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
         }
     }
 }
